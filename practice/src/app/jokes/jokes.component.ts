46829d5c6b855e1220e3219dca379062129a3262
Ian Szczesniak <ianszcze@buffalo.edu>
2019-01-06 17:48:46 -0500

    string changes

--- practice/src/app/jokes/jokes.component.ts
+++ practice/src/app/jokes/jokes.component.ts
@@ -8,7 +8,7 @@ import { JokeServiceService } from '../../services/joke-service.service';
   styleUrls: ['./jokes.component.css']
 })
 export class JokesComponent implements OnInit {
-
+  variable: string = "pinching";
   constructor(private JokeService: JokeServiceService) { }
 
   ngOnInit() {
